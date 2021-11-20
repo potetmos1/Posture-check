7cb028183b63b0d78d1c552fdfb51edb89e0e9d5
Petter Sæterøy <80422641+potetmos1@users.noreply.github.com>
2021-11-20 20:48:33 +0100

    Uploaded project

---
+++ postureCheck.c
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mmsystem.h>
+
+//compile with "gcc -lwinmm  postureCheck.c -o postureCheck.exe" for windows
+//compile with "gcc -lwinmm postureCheck.c -o posturecheck " for linux
+
+int main(int argc, char const *argv[])
+{
+    int low, high, time;
+
+    printf("Write in the minumum amount of minutes:\n");
+    scanf("%d", &low);
+
+    printf("Write in the maximum amount of minutes:\n");
+    scanf("%d", &high);
+
+    while(1){
+        time = low + (rand() % (high - low + 1));
+        sleep(time * 60);
+        printf("POSTURE CHECK!");
+        PlaySound("posturecheck.wav", NULL, SND_ASYNC);
+    }
+
+    return 0;
+}
